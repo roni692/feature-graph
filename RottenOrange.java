5a7154a2759b3d09602aa554e927e7332c8ba611
Ronish <ronishpanjwani@gmail.com>
2025-05-19 10:44:25 -0400

    Rotten Orange Change

---
+++ RottenOrange.java
@@ -0,0 +1,47 @@
+//Rotten Orange
+public class RottenOrange{
+
+	public int orangesRotting(int[][] grid){
+		if(grid == null || grid.length == 0)
+			return -1;
+	
+		int m = grid.length,n = grid[0].length;
+		int freshCount = 0;
+		Queue<int[]> rottenQueue = new LinkedList<>();
+	
+		//Count Fresh Orange and enqueue rotten oranges
+		for(int i = 0;i < m:i++){
+			for(int j = 0; j < n; j++){
+				if(grid[i][j] == 1){
+					freshCount++;
+				}else if(grid[i][j] == 2){
+					rottenQueue.offer(new int[]{i,j});
+				}
+			}
+		}
+		if(freshCount == 0) return 0;// No Fresh Oranges
+	
+		int minutes = 0;
+	
+		int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+	
+		while(!rottenQueue.isEmpty()){
+			int size = rottenQueue.size();
+			for(int i = 0;i < size;i++){
+				int[] rotten = rottenQueue.poll();
+			for(int[] dir : directions){
+				int x = rotten[0] + dir[0];
+				int y = rotten[1] + dir[1];
+			
+			if(x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1){
+				grid[x][y] = 2;
+				freshCount--;
+				rottenQueue.offer(new int[]{x,y});
+			}	
+		}
+	}
+		minutes;
+	}
+		return freshCount == 0 ? minutes - 1 : -1;
+	}
+}		
